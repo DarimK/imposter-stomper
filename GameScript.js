2d3c31a54718e9c754cadbf90ac624a8c5d21a03
Darim Khan <61602935+DarimK@users.noreply.github.com>
2023-11-01 04:29:08 -0400

    Fixed touchscreen issue

--- GameScript.js
+++ GameScript.js
@@ -106,7 +106,7 @@ function posToKeys(pos) {
     }
 }
 
-arena.addEventListener('ontouchstart', function (event) {
+arena.addEventListener('touchstart', function (event) {
 	let mouseX = (event.clientX - arena.offsetLeft) + arenaWidth / 2;
 	let mouseY = (event.clientY - arena.offsetTop) + arenaHeight / 2;
 	mousePos = [[mouseX, mouseY], null];
@@ -115,13 +115,13 @@ arena.addEventListener('ontouchstart', function (event) {
 	document.getElementById('circle').style.display = "block";
 });
 
-document.addEventListener('ontouchend', function () {
+document.addEventListener('touchend', function () {
 	mousePos = null;
 	keys = [0, 0, 0, 0, 0, 0, 0, 0];
 	document.getElementById('circle').style.display = "none";
 });
 
-arena.addEventListener('ontouchmove', function (event) {
+arena.addEventListener('touchmove', function (event) {
 	if (mousePos) {
 		let mouseX = (event.clientX - arena.offsetLeft) + arenaWidth / 2;
 		let mouseY = (event.clientY - arena.offsetTop) + arenaHeight / 2;
